34cdee452ed94538818929dbb92b04bcd64d66e7
Balakumar <balakumar59192@gmail.com>
2025-02-06 19:26:05 +0530

    Feb6

--- tests/Sample_test.spec.js
+++ tests/Sample_test.spec.js
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-
-test("Login Test",async ({browser}) => {
+    test("Login Test",async ({browser}) => {
 const context = await browser.newContext({ headless: false });
 const page = await context.newPage();
 
@@ -9,17 +8,32 @@ let pwd = "secret_sauce";
 let usr = "standard_user";
 
 await page.goto('https://www.saucedemo.com');
-
 await page.pause();
+
 // using object locator
-await expect(page.locator("//*[@id='root']//div[contains(text(),'Swag Labs')]")).toHaveText('Swag Labs');
+//await expect(page.locator("//*[@id='root']//div[contains(text(),'Swag Labs')]")).toHaveText('Swag Labs');
+if(page.$('text=Swag Labs')){
 
 await page.fill('[data-test="username"]', usr);
 
 await page.locator('[data-test="username"]').press('Tab');
-
+ 
 await page.locator("//*[@name ='password']").fill(pwd);
 
 await page.locator('input:has-text("LOGIN")').click();
 
-})
+await page.waitForTimeout(3000);
+
+await expect(page.locator('input:has-text("LOGIN")')).toBeHidden();
+
+await expect(page.locator('//*[text()="Products"]')).toBeVisible();
+
+await expect(page).toHaveScreenshot();
+
+} 
+else {
+    console.log("Not Found");
+}
+
+});
+
